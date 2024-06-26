40c28a995210a67608b379b9eb9054d6ebd9b819
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-06-26 09:57:30 +0700

    Create J02104_DANH_SACH_CANH.java

---
+++ MANG/J02104_DANH_SACH_CANH.java
@@ -0,0 +1,14 @@
+package MANG;
+import java.util.Scanner;
+public class J02104_DANH_SACH_CANH.{
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                int x = sc.nextInt();
+                if(x==1 && i<j) System.out.printf("(%d,%d)\n", i, j);
+            } 
+        }
+    }
+}
