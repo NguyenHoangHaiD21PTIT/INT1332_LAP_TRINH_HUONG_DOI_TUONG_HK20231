2350043de680ead5e50460b68beaaf261e7ec564
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-06-26 09:18:43 +0700

    Update J02027_KHOANG_CACH_NHO_HON_K.java

--- MANG/J02027_KHOANG_CACH_NHO_HON_K.java
+++ MANG/J02027_KHOANG_CACH_NHO_HON_K.java
@@ -5,29 +5,37 @@ SX. Xet cac so sau a[i]: x sao cho x - a[i] <k --> x <= k + a[i]- 1
 --> Tim vi tri dau tien > k + a[i] - 1 --> upperbound --> Lui pos di 1
 --> Cac so thoa man [i+1;pos-1] */
 public class J02027_KHOANG_CACH_NHO_HON_K {
-    public static int upper_bound(int l, int r, int[] a, int x) {
-        int mid = 0;
-        while (l < r) {
-            mid = (l + r) / 2;
-            if (a[mid] <= x) l = mid + 1;
-            else r = mid;
+    public static int upperBound(long[] arr, int start, int end, long value) {
+        int low = start;
+        int high = end;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] <= value) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
         }
-        return l;
+        return low;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt(); 
+        long t = sc.nextLong();
         while (t-- > 0) {
-            int n = sc.nextInt(), k = sc.nextInt();
-            int[] a = new int[n];      
-            for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+            long n = sc.nextLong();
+            long k = sc.nextLong();
+            long[] a = new long[(int)n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextLong();
+            }
             Arrays.sort(a);
-            int ans = 0;      
+            long ans = 0;
             for (int i = 0; i < n - 1; i++) {
-                int pos = upper_bound(i + 1, n, a, k + a[i] - 1);
-                int socap = (pos - 1) - (i + 1) + 1;
+                int pos = upperBound(a, i + 1, (int)n, k + a[i] - 1);
+                long socap = (pos - 1) - (i + 1) + 1;
                 ans += socap;
-            }       
+            }
             System.out.println(ans);
         }
     }
