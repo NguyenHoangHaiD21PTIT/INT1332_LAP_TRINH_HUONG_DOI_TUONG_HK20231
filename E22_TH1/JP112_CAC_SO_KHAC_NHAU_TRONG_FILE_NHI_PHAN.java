6afbb96c21b7f3ea513a4db762ce09a4c06a3feb
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-10-13 12:59:52 +0700

    Update JP112_CAC_SO_KHAC_NHAU_TRONG_FILE_NHI_PHAN.java

--- E22_TH1/JP112_CAC_SO_KHAC_NHAU_TRONG_FILE_NHI_PHAN.java
+++ E22_TH1/JP112_CAC_SO_KHAC_NHAU_TRONG_FILE_NHI_PHAN.java
@@ -1,5 +1,21 @@
+package E22_TH1;
 import java.util.*;
+import java.io.*;
 public class JP112_CAC_SO_KHAC_NHAU_TRONG_FILE_NHI_PHAN {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException{
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream("DATA.in"));
+        ArrayList<String>a = (ArrayList<String>)in.readObject();
+        TreeSet<Integer>ts = new TreeSet<>();
+        for (String s : a) {
+            String []tmp = s.trim().split("\\s+");
+            for(String x: tmp){
+                try {
+                    int number = Integer.parseInt(x);
+                    ts.add(number); 
+                } catch (NumberFormatException e) {
+                }
+            }   
+        }
+        for (int x: ts) System.out.println(x);
     }
 }
