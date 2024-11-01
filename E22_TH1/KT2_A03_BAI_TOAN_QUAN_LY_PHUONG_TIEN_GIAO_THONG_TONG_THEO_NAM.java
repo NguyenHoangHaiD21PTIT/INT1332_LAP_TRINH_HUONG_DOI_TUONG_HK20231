8cb286f2a5a1fbcdba9b4ffbc0c71cbabb0979d4
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-11-01 08:32:01 +0700

    Create KT2_A03_BAI_TOAN_QUAN_LY_PHUONG_TIEN_GIAO_THONG_TONG_THEO_NAM.java

---
+++ E22_TH1/KT2_A03_BAI_TOAN_QUAN_LY_PHUONG_TIEN_GIAO_THONG_TONG_THEO_NAM.java
@@ -0,0 +1,6 @@
+import java.io.*;
+import java.util.*;
+public class KT2_A03_BAI_TOAN_QUAN_LY_PHUONG_TIEN_GIAO_THONG_TONG_THEO_NAM {
+    public static void main(String[] args) throws IOException{
+    }
+}
