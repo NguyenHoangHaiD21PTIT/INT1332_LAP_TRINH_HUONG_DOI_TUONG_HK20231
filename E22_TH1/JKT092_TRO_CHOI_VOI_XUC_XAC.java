0ba82bff4099c716a3c93495b5a528e1357c9af8
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-11-01 18:41:53 +0700

    Create JKT092_TRO_CHOI_VOI_XUC_XAC.java

---
+++ E22_TH1/JKT092_TRO_CHOI_VOI_XUC_XAC.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class JKT092_TRO_CHOI_VOI_XUC_XAC {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //
+        }
+    }
+}
