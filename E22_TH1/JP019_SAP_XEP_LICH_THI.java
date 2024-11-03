8caeb56c6b5d6e4540358e7fdd90a7510c76732f
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-11-03 09:56:20 +0700

    Delete E22_TH1/JP019_SAP_XEP_LICH_THI.java

--- E22_TH1/JP019_SAP_XEP_LICH_THI.java
+++
@@ -1,8 +0,0 @@
-package E22_TH1;
-import java.util.*;
-public class JP019_SAP_XEP_LICH_THI {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-       
-    }
-}
