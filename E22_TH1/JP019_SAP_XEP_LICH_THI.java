1714e2102bd4fee56b3b5d84cf2edcaadb399a22
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-11-03 08:59:43 +0700

    Create JP019_SAP_XEP_LICH_THI.java

---
+++ E22_TH1/JP019_SAP_XEP_LICH_THI.java
@@ -0,0 +1,8 @@
+package E22_TH1;
+import java.util.*;
+public class JP019_SAP_XEP_LICH_THI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       
+    }
+}
