0d30a5c78e92e5e67678fe4fe82bb3a7f77fa9df
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-11-16 09:07:23 +0700

    Update and rename TONG_VA_TICH.java to JP340_TONG_VA_TICH_SO_KHAC_NHAU.java

--- THI_CUOI_KY_D21_Demo/JP340_TONG_VA_TICH_SO_KHAC_NHAU.java
+++ THI_CUOI_KY_D21_Demo/JP340_TONG_VA_TICH_SO_KHAC_NHAU.java
@@ -2,7 +2,7 @@ package THI_CUOI_KY_D21_Demo;
 import java.util.*;
 import java.io.*;
 import java.math.BigInteger;
-public class TONG_VA_TICH {
+public class JP340_TONG_VA_TICH_SO_KHAC_NHAU {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(new File("DATA.in"));
         while(sc.hasNextBigInteger()){
