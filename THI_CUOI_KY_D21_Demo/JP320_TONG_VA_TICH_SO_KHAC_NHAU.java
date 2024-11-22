7c22705412f24088130a54cd3fbeee1650c80e00
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-11-22 08:43:39 +0700

    Update and rename JP340_TONG_VA_TICH_SO_KHAC_NHAU.java to JP320_TONG_VA_TICH_SO_KHAC_NHAU.java

--- THI_CUOI_KY_D21_Demo/JP320_TONG_VA_TICH_SO_KHAC_NHAU.java
+++ THI_CUOI_KY_D21_Demo/JP320_TONG_VA_TICH_SO_KHAC_NHAU.java
@@ -2,7 +2,7 @@ package THI_CUOI_KY_D21_Demo;
 import java.util.*;
 import java.io.*;
 import java.math.BigInteger;
-public class JP340_TONG_VA_TICH_SO_KHAC_NHAU {
+public class JP320_TONG_VA_TICH_SO_KHAC_NHAU {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(new File("DATA.in"));
         while(sc.hasNextBigInteger()){
