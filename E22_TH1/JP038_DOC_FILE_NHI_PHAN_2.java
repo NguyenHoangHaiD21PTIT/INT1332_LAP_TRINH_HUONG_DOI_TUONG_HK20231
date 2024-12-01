636160d17a787ad58bc5324b8937e1aa86558a88
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-12-01 19:29:40 +0700

    Delete E22_TH1/JP038_DOC_FILE_NHI_PHAN_2.java

--- E22_TH1/JP038_DOC_FILE_NHI_PHAN_2.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class JP038_DOC_FILE_NHI_PHAN_2 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        //
-        }
-    }
-}
