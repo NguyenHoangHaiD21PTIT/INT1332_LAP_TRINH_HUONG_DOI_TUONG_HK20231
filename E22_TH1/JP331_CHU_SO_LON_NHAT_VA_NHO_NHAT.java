d9579dc84d5ae88e2740cbcb4078922532ebffdd
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-03-25 16:26:31 +0700

    Update JP331_CHU_SO_LON_NHAT_VA_NHO_NHAT.java

--- E22_TH1/JP331_CHU_SO_LON_NHAT_VA_NHO_NHAT.java
+++ E22_TH1/JP331_CHU_SO_LON_NHAT_VA_NHO_NHAT.java
@@ -1,6 +1,18 @@
 import java.io.*;
 import java.util.*;
 public class JP331_CHU_SO_LON_NHAT_VA_NHO_NHAT {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();  
+        while (t-- > 0) {
+            String N = sc.next();  
+            char maxDigit = '0';
+            char minDigit = '9';
+            for (char digit : N.toCharArray()) {
+                if (digit > maxDigit) maxDigit = digit;
+                if (digit < minDigit) minDigit = digit;
+            }
+            System.out.println(maxDigit + " " + minDigit);
+        }
     }
 }
