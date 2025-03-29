8309d793b8d7efc711d4382a9fe143133c883198
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-03-29 10:42:52 +0700

    Update JP006_LON_NHAT_NHO_NHAT.java

--- E22_TH1/JP006_LON_NHAT_NHO_NHAT.java
+++ E22_TH1/JP006_LON_NHAT_NHO_NHAT.java
@@ -1,6 +1,13 @@
 import java.io.*;
 import java.util.*;
 public class JP006_LON_NHAT_NHO_NHAT {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args){
+        long maxVal = Long.MIN_VALUE, minVal = Long.MAX_VALUE;
+        while (sc.hasNextLong()) {
+            long x = sc.nextLong();
+            if (x > maxVal) maxVal = x;
+            if (x < minVal) minVal = x;
+        }
+        System.out.println(maxVal + " " + minVal);        
     }
 }
