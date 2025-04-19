220e7cbc29eb0973bf6500375422373e753bda8a
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-04-19 07:49:28 +0700

    Sửa logic sắp xếp

--- VAO_RA_FILE/J07059_DANH_SACH_CA_THI.java
+++ VAO_RA_FILE/J07059_DANH_SACH_CA_THI.java
@@ -1,52 +1,50 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.*;
-class CaThi{
+class CaThi implements Comparable<CaThi> {
     private String id, ngaythi, giothi, room;
-    private int tongngay, tonggio;
-    public CaThi(int p, String ngaythi, String giothi, String room){
+    private int ngay, thang, nam, gio, phut;
+    public CaThi(int p, String ngaythi, String giothi, String room) {
         this.id = "C" + String.format("%03d", p);
         this.ngaythi = ngaythi;
         this.giothi = giothi;
         this.room = room;
-        String []a = this.ngaythi.split("/");
-        String []b = this.giothi.split(":");
-        for(String x: a) this.tongngay+=Integer.parseInt(x);
-        for(String x: b) this.tonggio+=Integer.parseInt(x);
+
+        String[] d = ngaythi.split("/");
+        this.ngay = Integer.parseInt(d[0]);
+        this.thang = Integer.parseInt(d[1]);
+        this.nam = Integer.parseInt(d[2]);
+
+        String[] t = giothi.split(":");
+        this.gio = Integer.parseInt(t[0]);
+        this.phut = Integer.parseInt(t[1]);
     }
-    public int tongngay(){
-        return this.tongngay;
+    @Override
+    public int compareTo(CaThi other) {
+        if (this.nam != other.nam) return this.nam - other.nam;
+        if (this.thang != other.thang) return this.thang - other.thang;
+        if (this.ngay != other.ngay) return this.ngay - other.ngay;
+        if (this.gio != other.gio) return this.gio - other.gio;
+        if (this.phut != other.phut) return this.phut - other.phut;
+        return this.id.compareTo(other.id);
     }
-    public int tonggio(){
-        return this.tonggio;
-    }
-    public String getid(){
-        return this.id;
-    }
-    public String toString(){
-        return this.id + " " + this.ngaythi + " " + this.giothi + " " + this.room;
+    @Override
+    public String toString() {
+        return id + " " + ngaythi + " " + giothi + " " + room;
     }
 }
+
 public class J07059_DANH_SACH_CA_THI {
-    public static void main(String[] args) {
-        try {
-            Scanner sc = new Scanner(new File("CATHI.in"));
-            int n = sc.nextInt();
-            sc.nextLine();
-            CaThi []a = new CaThi[n];
-            for(int i = 0;i<n;i++){
-                a[i] = new CaThi(i + 1, sc.nextLine(), sc.nextLine(), sc.nextLine());
-            }
-            Arrays.sort(a, new Comparator<CaThi>(){
-                public int compare(CaThi a, CaThi b){
-                    if(a.tongngay()!= b.tongngay()) return a.tongngay() - b.tongngay();
-                    if(a.tonggio()!=b.tonggio()) return a.tonggio() - b.tonggio();
-                    return a.getid().compareTo(b.getid());
-                }
-            });
-            for(CaThi x: a) System.out.println(x);
-        } catch (FileNotFoundException e){
-            e.printStackTrace();
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(new File("CATHI.in"));
+        int n = Integer.parseInt(sc.nextLine());
+        CaThi[] a = new CaThi[n];
+        for (int i = 0; i < n; i++) {
+            String ngay = sc.nextLine();
+            String gio = sc.nextLine();
+            String room = sc.nextLine();
+            a[i] = new CaThi(i + 1, ngay, gio, room);
         }
+        Arrays.sort(a); 
+        for (CaThi x : a) System.out.println(x);
     }
 }
