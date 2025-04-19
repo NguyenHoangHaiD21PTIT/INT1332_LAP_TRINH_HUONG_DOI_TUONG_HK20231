dc8421216bfbb2952fdf588837ab3d042902f728
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-04-19 08:01:19 +0700

    Update J07081_SAP_XEP_DANH_SACH_SINH_VIEN.java

--- VAO_RA_FILE/J07081_SAP_XEP_DANH_SACH_SINH_VIEN.java
+++ VAO_RA_FILE/J07081_SAP_XEP_DANH_SACH_SINH_VIEN.java
@@ -1,49 +1,39 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.*;
-class Student{
-    public String id, name, phone, mail;
-    public Student(String id, String name, String phone, String mail){
+class Student implements Comparable<Student> {
+    private String id, name, phone, mail;
+    private String ho, tendem, ten;
+    public Student(String id, String name, String phone, String mail) {
         this.id = id;
-        this.name = name;
+        this.name = name.trim();
         this.phone = phone;
         this.mail = mail;
+        String[] parts = this.name.split("\\s+");
+        this.ho = parts[0];
+        this.ten = parts[parts.length - 1];
+        this.tendem = "";
+        for (int i = 1; i < parts.length - 1; i++) this.tendem += parts[i] + " ";
+        this.tendem = this.tendem.trim(); 
     }
-    public String getmsv(){
-        return this.id;
+    @Override
+    public int compareTo(Student other) {
+        if (!this.ten.equals(other.ten)) return this.ten.compareTo(other.ten);
+        if (!this.ho.equals(other.ho)) return this.ho.compareTo(other.ho);
+        if (!this.tendem.equals(other.tendem)) return this.tendem.compareTo(other.tendem);
+        return this.id.compareTo(other.id);
     }
-    public String getname(){
-        return this.name;
-    }
-    public String toString(){
-        return this.id + " " + this.name + " " + this.phone + " " + this.mail;
+    @Override
+    public String toString() {
+        return id + " " + name + " " + phone + " " + mail;
     }
 }
 public class J07081_SAP_XEP_DANH_SACH_SINH_VIEN {
-    public static void main(String[] args) {
-        try {
-            Scanner sc = new Scanner(new File("SINHVIEN.in"));
-            int n = sc.nextInt();
-            sc.nextLine();
-            Student []a = new Student[n];
-            for(int i = 0;i<n;i++){
-                a[i] = new Student(sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextLine());
-            }
-            Arrays.sort(a, new Comparator<Student>(){
-                public int compare(Student a, Student b){
-                    if(!a.getname().equals(b.getname())){
-                        String []x = a.getname().trim().split("\\s+");
-                        String []y = b.getname().trim().split("\\s+");
-                        if(!x[x.length - 1].equals(y[y.length - 1])) return x[x.length - 1].compareTo(y[y.length - 1]);
-                        if(!x[0].equals(y[0])) return x[0].compareTo(y[0]);
-                        return x[1].compareTo(y[1]);
-                    }
-                    return a.getmsv().compareTo(b.getmsv());
-                }
-            });
-            for(Student x: a) System.out.println(x);
-        } catch (FileNotFoundException e){
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(new File("SINHVIEN.in"));
+        int n = Integer.parseInt(sc.nextLine());
+        Student[] a = new Student[n];
+        for (int i = 0; i < n; i++) a[i] = new Student(sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextLine());
+        Arrays.sort(a);  
+        for (Student x : a) System.out.println(x);
     }
 }
