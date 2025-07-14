b178b01847563be4cbd78458c5d06c3e5bb09103
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-15 04:46:41 +0700

    Add files via upload

---
+++ KIEM_TRA_23_09_CA_13H_CO_VAN_ANH/KT1_17_TICH_HAI_MA_TRAN.java
@@ -0,0 +1,52 @@
+import java.util.*;
+class Matran{
+    private int n;
+    private int [][]a;
+    public Matran(int n){
+        this.n = n;
+        this.a = new int[n][n];
+    }
+    public int [][] getMatran(){
+        return this.a;
+    }
+    public void input(Scanner sc){
+        for(int i = 0;i<this.n;i++){
+            for(int j = 0;j<this.n;j++) this.a[i][j] = sc.nextInt();
+        }
+    }
+    public int getKT(){
+        return this.n;
+    }
+    public void out(){
+        for(int i = 0;i<this.n;i++){
+            for(int j = 0;j<this.n;j++) System.out.print(this.a[i][j] + " ");
+            System.out.println();
+        }
+    }
+    public Matran tich(int [][]b){
+        Matran res = new Matran(this.n);
+        for(int i = 0;i<this.n;i++){
+            for(int j = 0;j<this.n;j++){
+                res.a[i][j] = 0;
+                for(int k = 0; k < this.n; k++) res.a[i][j] += this.a[i][k] * b[k][j];
+            }
+        }
+        return res;
+    }
+}
+public class KT1_17_TICH_HAI_MA_TRAN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //nhap kich thuoc
+        int n = sc.nextInt();
+        Matran m = new Matran(n);
+        //nhap ma tran a
+        m.input(sc);
+        Matran b = new Matran(m.getKT());
+        //nhap ma tran b
+        b.input(sc);
+        Matran h = m.tich(b.getMatran());
+        //viet ra ma tran tich
+        h.out();
+    }
+}
